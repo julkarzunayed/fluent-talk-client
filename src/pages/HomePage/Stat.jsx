41a9b1412d64401de3abb1779ca842fafaf3d596
julkarzunayed <zunayedjulkar@gmail.com>
2025-06-15 09:42:03 +0600

    added tutor and students numbers from the DB

--- src/pages/HomePage/Stat.jsx
+++ src/pages/HomePage/Stat.jsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { FaCommentMedical } from 'react-icons/fa';
 import { IoLanguageSharp } from 'react-icons/io5';
 import { LiaChalkboardTeacherSolid } from 'react-icons/lia';
 import { PiStudentDuotone } from 'react-icons/pi';
 
+
+
 const Stat = () => {
+    const [studentCount, setStudentCount ]= useState(0);
+    const [tutorCount, setTutorCount ]= useState(0);
+
+    useEffect(() => {
+        axios.get(`http://localhost:3000/user/student`)
+            .then(res => setStudentCount(res.data))
+        axios.get(`http://localhost:3000/user/tutor`)
+            .then(res => setTutorCount(res.data))
+    }, [])
+    // console.log(studentCount, tutorCount);
     return (
         <div className="flex p-5">
             <div className="stats shadow mx-auto">
@@ -12,8 +26,17 @@ const Stat = () => {
                         <LiaChalkboardTeacherSolid  size={40}/>
                     </div>
                     <div className="stat-title">Tutors</div>
-                    <div className="stat-value">31K</div>
+                    <div className="stat-value">{tutorCount}+</div>
                     <div className="stat-desc">Jan 1st - Feb 1st</div>
+                </div>
+
+                <div className="stat">
+                    <div className="stat-figure text-orange-600">
+                        <FaCommentMedical size={40}/>
+                    </div>
+                    <div className="stat-title">Reviews</div>
+                    <div className="stat-value">4,200</div>
+                    <div className="stat-desc">↗︎ 400 (22%)</div>
                 </div>
 
                 <div className="stat">
@@ -21,7 +44,7 @@ const Stat = () => {
                         <PiStudentDuotone  size={40}/>
                     </div>
                     <div className="stat-title">Learners</div>
-                    <div className="stat-value">4,200</div>
+                    <div className="stat-value">{studentCount}+</div>
                     <div className="stat-desc">↗︎ 400 (22%)</div>
                 </div>
 
@@ -29,7 +52,7 @@ const Stat = () => {
                     <div className="stat-figure text-orange-600">
                         <IoLanguageSharp size={40} />
                     </div>
-                    <div className="stat-title">Language</div>
+                    <div className="stat-title">Language <span>&#x1f1e7;&#x1f1e9;</span></div>
                     <div className="stat-value">1,200</div>
                     <div className="stat-desc">↘︎ 90 (14%)</div>
                 </div>
